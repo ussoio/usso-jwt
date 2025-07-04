02f521d9ebf078149bfc85cc82ccf720048996ac
Mahdi Kiani <mahdikiany@gmail.com>
2025-07-04 20:00:21 +0330

    V0.2.1 add test base

---
+++ tests/algorithm/test_base.py
@@ -0,0 +1,22 @@
+"""Tests for base algorithm."""
+
+from src.usso_jwt.algorithms import AbstractKey, convert_jwk_to_pem
+
+
+def test_load_jwt(rsa_jwk: dict | bytes):
+    """Test loading RSA key from PEM."""
+
+    key = AbstractKey.load(rsa_jwk)
+
+    assert hasattr(key, "key")
+    key = key.key
+    assert hasattr(key, "sign")
+    assert hasattr(key, "private_bytes")
+
+
+def test_jwt_to_pem(rsa_jwk: dict | bytes):
+    """Test loading RSA key from PEM."""
+
+    key = AbstractKey.load(rsa_jwk)
+    pem = convert_jwk_to_pem(rsa_jwk)
+    assert pem == key.public_pem()
