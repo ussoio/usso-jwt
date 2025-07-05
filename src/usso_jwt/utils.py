0f1a32ada4dfea7fce63b2c4d27cca9191e41b17
Mahdi Kiani <mahdikiany@gmail.com>
2025-07-05 10:12:40 +0000

    v0.2.2 base64 on bytes'

--- src/usso_jwt/utils.py
+++ src/usso_jwt/utils.py
@@ -1,11 +1,15 @@
 import base64
 
 
-def b64url_decode(data: str) -> bytes:
+def b64url_decode(data: str | bytes) -> bytes:
     padding = "=" * (-len(data) % 4)
+    if isinstance(data, bytes):
+        padding = padding.encode()
     return base64.urlsafe_b64decode(data + padding)
 
 
-def b64url_encode(data: bytes) -> str:
+def b64url_encode(data: str | bytes) -> str:
     """Base64url encode bytes."""
+    if isinstance(data, str):
+        data = data.encode()
     return base64.urlsafe_b64encode(data).rstrip(b"=").decode()
